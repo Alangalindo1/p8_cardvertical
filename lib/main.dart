c6339a91be9507d9a1d17e7a57c277e7a32c0273
ALAN_OSVALDO_GALINDO_ACEVEDO <a.21308051280475@cbtis128.edu.mx>
2024-08-14 04:03:49 +0000

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:vertical_card_pager/vertical_card_pager.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: VerticalCardPagerDemo(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class VerticalCardPagerDemo extends StatefulWidget {
+  @override
+  _VerticalCardPagerDemoState createState() => _VerticalCardPagerDemoState();
+}
+
+class _VerticalCardPagerDemoState extends State<VerticalCardPagerDemo> {
+  final List<String> titles = [
+    "RED",
+    "YELLOW",
+    "CYAN",
+    "BLUE",
+    "GREY",
+  ];
 
   @override
   Widget build(BuildContext context) {
+    final List<Widget> images = [
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.cyan,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+      ),
+      Container(
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+      ),
+    ];
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text(
+          'Card vertical Galindo',
+          style: TextStyle(
+              color: Color(0xff000000), backgroundColor: Colors.lightGreen),
+        ),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: SafeArea(
+        child: Expanded(
+          child: Container(
+            child: VerticalCardPager(
+              textStyle:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              titles: titles,
+              images: images,
+              onPageChanged: (page) {},
+              align: ALIGN.CENTER,
+              onSelectedItem: (index) {},
+            ),
+          ),
         ),
       ),
     );
